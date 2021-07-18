d94c6611f18da1261737aada5f88021742265cb2
Harry-Liversedge <Harry.Liversedge@ibm.com>
2021-07-18 22:58:48 +0100

    add unary request to grpc service

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import { ArticlesPageRequest } from "./proto/articlespb/articles_pb";
+import { ArticlesPageServiceClient } from "./proto/articlespb/articles_grpc_web_pb";
+
+import "./App.css";
+
+var client = new ArticlesPageServiceClient("http://localhost:10000");
 
 function App() {
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    getArticles();
+  }, []);
+
+  const getArticles = () => {
+    var request = new ArticlesPageRequest();
+    var metadata = { "custom-header-1": "value1" };
+    request.setQueryString("Quantum Mechanics");
+    request.setPage(1);
+
+    client.getArticles(request, metadata, function (err, response) {
+      if (err) {
+        console.log(err.code);
+        console.log(err.message);
+      } else {
+        setArticles(response.getResultsList());
+      }
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getArticles}>Click to load articles</button>
+      {articles?.map((article) => JSON.stringify(article, undefined, 2))}
     </div>
   );
 }
